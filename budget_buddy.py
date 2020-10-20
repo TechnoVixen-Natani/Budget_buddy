9b89c794a274f9bd47a23fce57b58aaf2838b422
TechnoVixen-Natani <fayth.grayson@gmail.com>
2020-10-19 22:10:14 -0500

    Update budget_buddy.py
    Assisted by other devs, changed code for some functions to be more streamline. Omitted conflicting code. Moved code in a more logical fashion.

--- budget_buddy.py
+++ budget_buddy.py
@@ -17,33 +17,6 @@ import sqlite3
 from datetime import datetime
 from sqlite3 import Error
 
- 
-def deposits():
-    """Deposits function so we can call it later on"""
-    deposits = int(float(input('Please enter deposit amount:' )))                       
-    return deposits
- 
-def expenses():
-    """Expenses function to be used later on as well"""
-    expenses = int(float(input('Please enter expense amount:')))
-    return expenses
- 
-def balance():
-    """Balance function to be used later on as well"""
-    balance = (deposits() + cash_on_hand()) - expenses()
-    return balance
-
-
-def cash_on_hand():
-    """
-    Cash_on_hand function, this is asking how much money not in bank
-    accounts do you have.
-    """
-    cash_on_hand = int(float(input('How much money do you have on hand:')))
-    return cash_on_hand
-
-
-#####Program is still a WIP. Changes will be made.######
 
 """Current month with %B argument,which displays entire month name"""
 current_month = datetime.now().strftime('%B')
@@ -53,23 +26,7 @@ current_month = datetime.now().strftime('%B')
  Not used as of yet on 2020-10-17, but will be as the program develops."""
 current_time = datetime.now().strftime('%Y-%m-%d %H:%M')
 
-######Not in use yet######
-# def monthly_chart():
-    # my_data = [expenses and deposits here]
-    # my_labels = 'Expenses', 'Deposits'
-    # plt.pie(my_data, labels=my_labels,autopct='%1.1f%%')
-    # plt.title('{}'.format(current_month))
-    # plt.axis('equal')
-    # plt.show()
-    
-# menu = ("1.Deposit\n"
-       # "2.Expense\n"
-       # "3.Cash on hand\n"
-       # "4.Balance\n"
-       # "5.Create Chart\n"
-       # "6.Quit\n")
-#################################################
-    
+
 """Using os module to determine the current users desktop path so we can 
 make the budget database file on the desktop so it is easier to find.
 Sqlite3 module and Error module as well to create a connection to the 
@@ -94,11 +51,15 @@ def sql_connection(db_file):
     finally:
         if not conn:
             conn.close()
-        return conn
+        
 
 def monthly_table(conn):
     cursorObj = conn.cursor()
+<<<<<<< Updated upstream
     cursorObj.execute(f"CREATE TABLE {current_month}(Deposit integer, Expenses integer, Cash_On_Hand integer, Balance integer, Date text)")
+=======
+    cursorObj.execute(f"CREATE TABLE {current_month} (Deposit integer, Expense_Amount integer,Expense_Type text, Cash_on_Hand integer, Balance integer, Date text)")
+>>>>>>> Stashed changes
     conn.commit()
 conn = sql_connection(default_path)
 if conn:
@@ -108,4 +69,79 @@ else:
 
 if __name__ == '__main__':
     sql_connection(default_path)
+
+#Program stops if table already exists. Need to fix this!!
+
+total_money = 0
+
+
+def deposits():
+    """Deposits function so we can call it later on"""
+    return float(input('Please enter deposit amount:'))
+
+
+def input_expense_item() -> tuple:
+    """
+    Prompt the user to enter an expense type, and the amount for the expense
+    :return: Tuple - expense type, amount
+    """
+
+    expense_type = input("What is the expense item?\n").capitalize()
+    cost = None
+
+    # must ensure that the user provides a valid number! In this case, a valid decimal!
+    while not cost:
+        temp = input(f"How much was '{expense_type}'?\n")
+
+        try:
+            cost = float(temp)
+
+        except ValueError:
+            print(f"{temp} - was an invalid entry. Please make sure you entered a valid number")
+
+    # This is how to return a tuple. This means you can assign two variables when calling this function as seen below
+    return expense_type, cost
+
+
+def balance(expense_amount: int):
+    """Balance function to be used later on as well"""
+    return total_money - expense_amount
+
+
+def cash_on_hand():
+    """
+    Cash_on_hand function, this is asking how much money not in bank
+    accounts do you have.
+    """
+    return float(input('How much money do you have on hand:'))
+
+
+while True:
+    menu_option = input("What option do you want to do?\n 1: Enter deposit\n 2: Enter expenses\n")
+    if menu_option == "1":
+        deposit_amount = deposits()
+        total_money += deposit_amount
+
+    elif menu_option == "2":
+        new_expense_type, new_expense_amount = input_expense_item()
+        print(f"The user wants to add {new_expense_type} for ${new_expense_amount}")
+        current_balance = balance(new_expense_amount) + cash_on_hand()
+        total_money = current_balance
+        print(f"Total balance is ${total_money}")
+
+
+#####Program is still a WIP. Changes will be made.######
+
+
+
+######Not in use yet######
+# def monthly_chart():
+    # my_data = [expenses and deposits here]
+    # my_labels = 'Expenses', 'Deposits'
+    # plt.pie(my_data, labels=my_labels,autopct='%1.1f%%')
+    # plt.title('{}'.format(current_month))
+    # plt.axis('equal')
+    # plt.show()    
+#################################################
+
 
